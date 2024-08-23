ded4b344f1078f99d43e99e741041ac44765cdf7
FakeGath <givanalvaradoath@gmail.com>
2024-08-22 18:22:21 -0600

    Solution for Huge loops

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//library that includes basic commands in c++
+#include <iostream>
+
+int main(){
+
+    for (size_t i{0}; i < 1000000; i++)
+    {
+        //std::cout<<"i: "<<i<<std::endl;
+    }
+    std::cout<<"Done!"<<std::endl;
+    return 0;
+}
+    
